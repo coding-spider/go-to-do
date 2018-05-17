b34f568046f356899b877a9bbc1aaf0fe22a76b4
rohan <rohan@retailio.in>
2018-05-17 16:35:14 +0000

    delete tasklist by id

--- server/models/task.go
+++ server/models/task.go
@@ -6,7 +6,7 @@ type Task struct {
     Name string   `json:"name,omitempty"`
     Description  string   `json:"description,omitempty"`
     Status bool `json:"status,omitempty"`
-    TaskListId   string `json:"id_task_list,omitempty"`
+    TaskListId   int `json:"taskListId,omitempty"`
 }
 
 var Tasks []Task
--- server/routes/routes.go
+++ server/routes/routes.go
@@ -5,7 +5,7 @@ import (
     "github.com/gorilla/mux"
     "net/http"
     "strconv"
-    // "fmt"
+    "fmt"
 
     "go-to-do/server/models"
 )
@@ -30,12 +30,13 @@ func CreateTaskForATaskList(w http.ResponseWriter, r *http.Request) {
     params := mux.Vars(r)
     var newTask models.Task
     _ = json.NewDecoder(r.Body).Decode(&newTask)
-    newTask.TaskListId = params["id"]
+    newTask.TaskListId, _ = strconv.Atoi(params["id"])
     createdTask := models.CreateNewTask(newTask)
     w.Header().Set("Content-Type", "application/json")
     json.NewEncoder(w).Encode(createdTask)
 }
 
+// Fetching Task List by Id
 func GetTaskListById(w http.ResponseWriter, r *http.Request) {
     params := mux.Vars(r)
     taskId, _ := strconv.Atoi(params["id"])
@@ -49,11 +50,38 @@ func GetTaskListById(w http.ResponseWriter, r *http.Request) {
     json.NewEncoder(w).Encode(nil)
 }
 
+
+// Delete Task List By Id
+func DeleteTaskListById(w http.ResponseWriter, r *http.Request) {
+    params := mux.Vars(r)
+    taskId, _ := strconv.Atoi(params["id"])
+
+    //Delete TaskList
+    for idx, item := range models.TaskLists {
+        if item.ID == taskId {
+            models.TaskLists = append(models.TaskLists[:idx], models.TaskLists[idx+1:]...)
+            break
+        }
+    }
+
+    //Delete Tasks
+    for idx, item := range models.Tasks {
+        if item.TaskListId == taskId {
+            models.Tasks = append(models.Tasks[:idx], models.Tasks[idx+1:]...)
+            break
+        }
+    }
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(true)
+}
+
 func GetRouter() *mux.Router {
     router := mux.NewRouter()
     router.HandleFunc("/tasklists", GetTaskLists).Methods("GET")
     router.HandleFunc("/tasklists/{id}", GetTaskListById).Methods("GET")
+    router.HandleFunc("/tasklists/{id}", DeleteTaskListById).Methods("DELETE")
     router.HandleFunc("/tasklists", CreateTaskList).Methods("POST")
     router.HandleFunc("/tasklists/{id}/createTask", CreateTaskForATaskList).Methods("POST")
+    fmt.Println("Server is running");
     return router
 }
